5f6261889c3e0b0ac7ebf2cb3a379176e7b66e9d
Chadive Sreenath <46984264malli@gmail.com>
2023-06-12 22:18:49 +0530

    adding new file

---
+++ signUp/signUp.js
@@ -0,0 +1,19 @@
+let Btn = document.getElementById("singUp");
+Btn.addEventListener("click", function () {
+  let emaildata = document.getElementById("Email").value;
+  let passworddata = document.getElementById("password").value;
+
+  let user =JSON.parse(localStorage.getItem("Singup")) || [];
+
+  let obj = {
+    Email: emaildata,
+    Password: passworddata,
+  };
+
+  user.push(obj)
+  localStorage.setItem("Singup",JSON.stringify(user));
+});
+
+function signUp(){
+  window.location.href =""
+}
