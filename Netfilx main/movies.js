cc916f8f5c5cbc5349dcefdfacab30c1f3eb87b6
Chadive Sreenath <46984264malli@gmail.com>
2023-06-14 16:05:14 +0530

    add new file in netflix

---
+++ Netfilx main/movies.js
@@ -0,0 +1,186 @@
+// let obj = [{Image:"https://occ-0-4994-2164.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABfmSHLYq43DDK7CRGJnBAJXy3Ym5_IfmT9JcydtFNTwjD8An2WZ28MWA77qrBvBQ6-SJIu8IqtrxfOXs8QUJOAPofIn-G6GWQJ44.jpg?r=7ba",
+// TextImage:"https://occ-0-4994-2164.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABdN2FOWbrl0gAOY-Zj9mA4lJOS6ITW1js-CmJotp7K0t7kBx-hY_bn8fik1-E-DxdiDQcVUb1aywKiS1WbBaGKHp9EFyTpLy6hemoyecMqXU.png?r=52d",
+// Title:"Tu Jhoothi Main Makkaar",
+// Text:"To earn extra cash, Mickey helps couples break up — but life gets complicated when he falls for Tinni, a career woman with an independent streak."},
+// {Image:"https://occ-0-2085-2164.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABdTn4K7W0-CgVQFdCWw7t2higWILlOvJzcsjsEdJVO0tgyl0oz9dC_vOXbFRrMy7ye5FCGsO5vwuK6WgMbyqDeRcjBxSvxOv8r1P.jpg?r=921",
+// TextImage:"https://occ-0-2085-2164.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABT2XDpkF2FBUbOM4UAmxCqt82aIjU0F8Lk5zTgY00_XaqZreO7Q3Kua8e0X4aIT8JQEsaie9if0mcr-DsJLL7nQJoH531tcPLq5mwfuSqbOV.png?r=f34",
+// Title:"The Boss Baby",
+// Text:"A kid finds himself at the center of a sinister corporate plot when his parents bring home a baby who only talks business when they're not around."},
+// {Image:"https://occ-0-2085-2164.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABY13a9uIuBg-slj3EoJwcK-4zCIVA-TqaeIGZ1gbdG7sZzos9NYU747YR5rnTLL8MzafxfEu5RL1aPVZQTPezRi-3AajqglDLDH5.jpg?r=67a",
+// TextImage:"https://occ-0-2085-2164.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABcSmNX2GOH06A6c1cawgA7OaFAfm26f3PI6SaSDfdwOtY6miT3OtwClEntGu4TmSpF3lmQt0UATBc0ZLVkhSnRarRojx7ol3RnybI_mN2gI4.png?r=64b",
+// Title:"STAND BY ME Doraemon 2",
+// Text:"Nobita travels to the future to show his beloved grandma his bride, but adult Nobita has fled his own wedding. Can he ever be a good husband to Shizuka?"},
+// {Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABdu2aiVyJyZLsUgBg2C7dcXtHgAOeb09p8dMfVC9q8R7t6ecgL4HvXdCZvzSuD4sG1TJK5HpUy8oU_lT-zPGeIQik-e21ijZ69ow.jpg?r=d3f",
+// TextImage:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABZJ-3sfIjAPjAfD9C1CYN-7ZajnMPtQtwanSLVRhcaw7bpRPSHCTOvSpAxxcjfZCsilXAG5lq3YXqSQJZmzgzVGGahKaTO3CtheXSFgDCD4A.png?r=11a",
+// Title:"Spider-Man: Into the Spider-Verse",
+// Text:"After being bitten by a radioactive spider, Brooklyn teen Miles Morales gets a crash course in web-slinging from his alternate-dimension counterparts."},
+// {Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABZ7WZcvYv5pBoXA-VQxDHm8VAjJEMYlkYb04pTJHO0iskjprLXx0LTsC7o9TOTpK3QRs7LQe7ELg2TxFFp8rPxRHE8E5pZlKltLN.jpg?r=c01",
+// TextImage:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABfCB-KEGke3ep2VrbsiTPnveldHVRBWsWAKcoGKj8uJwi2SMiQQ2cx8y5OJONkpNpSZzAvkT0P0PLLrVL8ly2WPOrhS7yhsfbsLQAXF-bp5Q.png?r=5a7",
+// Title:"Raw (Hindi)",
+// Text:"A jaded former intelligence agent is pulled back into action when an attack at a mall creates a tense hostage situation."},
+// {Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABbJZfhZjJFqfJa-5GPR1D-Pbfb2W6qqI1Gz4x6tiUzUln7ofoziE37sg8KqECz5GZq9nnv51i_5ayR0ITvamfj9Q0lySrQppX2rn.jpg?r=a4f",
+// TextImage:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABVUmIUkC4h9j0ZyzPzZAVCtIoNTQGjpI1xV23oItongLdZ1PJ0S6z255fF8yhDNy3G4f5vWY1mgfKo-BdUxIfMqHdKgmBzdWMZAdgrsEeuX5.png?r=9f3",
+// Title:"The Karate Kid",
+// Text:"In China, Dre learns to defend himself against a bully under the guidance of an unassuming kung fu master in this remake of the 1984 classic."},
+// {Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABfZRn19HBt7NTuSrxBvlnikg-3-xPeNEMALzVQe_Wm8LlSay1HOyewohNqSPEKgpgSh9GI5EqUPH6PnYY2dcpGC1kUmmNFKOLxAd.jpg?r=3e2",
+// TextImage:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABZ9OPtad-GSX1fsutWYTLWlsu3qeX_14uScvw5mrZxHzfPyUbQT5Yaxza7daVtNoW4gyYBO5XGgN_HUYq7_Y_kJQhksYv1ZyS8N3OhNnEmCG.png?r=a7a",
+// Title:"Lucy",
+// Text:"When a young American in Taiwan becomes an unwilling drug mule, the high-tech narcotics get released into her system and activate superhuman powers."}]
+
+
+// let image = document.querySelector(".conatiner")
+
+// obj.forEach(function(ele){
+//     let Imagedata = document.createElement("img");
+//     Imagedata.setAttribute("class","moviesImage");
+//     image.appendChild(Imagedata);
+//     Imagedata.src = ele.Image;
+    
+//     let imageLogo = document.createElement("img");
+//     imageLogo.setAttribute("class","movieName");
+//     Imagedata.appendChild(imageLogo);
+//     imageLogo.src = ele.TextImage
+
+// let movieTitle = document.createElement("h2");
+// movieTitle.setAttribute("class","title");
+// imageLogo.appendChild(movieTitle);
+// movieTitle.innerText = ele.Title
+// })
+
+
+let releasedInPast = 
+[{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABSuVAdpb5Qn3OXuzTs3FVDplvmSLLDY-oN3PwCxkQ_Zn50sO1ct69T08y_WhIZ7FCzIHfTVudSGqBp76-W0DCIpq8IUdow3PbPg.jpg?r=0c9",moviename:"Gumaraan"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABVqDWUme-UcYFwwG_jmQ4tSUGK0A2SHmWt3UYGmxTigIN60nS89sW9H-lvpwwFizX3bT7bKdJwDuPKNTZ1fLTx6sYw6AmdPKl0E.jpg?r=b0a",moviename:"Tu Joothi Main Makkaar"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABaQwzSZjssVIp_zhkfNZhrJxXFjUVNWnSF2ymjH1ginSQ4SfBG_BNAwbxW1Kfyza9TpMzkpUabCGyQ8cHvVig-_xczvdPednvnw.jpg?r=718",moviename:"Virupaksha"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABaAIKXB-pGekT_AK5YyKKbPSgbl2VOXV-nGARkGDhpybvTvQYwJ8JlPCZcqtDoHu1tEagfNGhAIl_pMLwIKvCN6yswYJBCBVazQ.jpg?r=9f9",moviename:"Shehzada"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABY33FjlYydrZmTddrkmVXH1JV797xiEIzIIM-BLMAPSyfVr3UM5Xx6WlkH-mtMqy1wVn7fpz5TbFggnldL-Ym0N89wHHuk62ov4.jpg?r=f02",moviename:"Major"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABXJAIeaLoxwHPTuaRMdQcZGKvt0dpsZyarjh0tAiL1tUsvG1ORcp_HtYafrA4ogzeuevvZOazZgqfMuUcYg-42VygOd0GEr3SrM.jpg?r=5e1",moviename:"GodFather"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABW8oaUjhggl8CMsts1Om0LmNK79aRi8JotMgVeTeq2vpBXYIvL3z_w827AUS34hFXUcxDnwcDsWARDbwTCX6vF4iQnBoGcZTBo8.jpg?r=357",moviename:"Thunivu"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABakLiEQqG26HokllH2Dps8eJpWnuxwjWYre8CMzIAn_HsccMC9dpvbgewZX9lABS683aQg08yn4v6aFrTxbREUEz5byiSLnUmL4.jpg?r=9de",moviename:"Bhool Bhukaiyaa 2"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABaI-GbxkRT96tEOt2FdPfU6mcFg873mHJqIjZvp0xxRw22ktyLoONTBi5VRz5rYw1EWh7ZUMqI65bvrKVlAPZhqmv3zrOnbdH5Q.jpg?r=420",moviename:"65"}]
+
+let comedyMovies =
+[{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABUuwa85zjDiGaUURrexIT0fgRozPOPQPJt2z4OtxHKJeaKSIBEngi4wDNklqQqm0x2oSDFgRaDg9fDeihLDMIl378es1pPtCI7g.jpg?r=860",moviename:"The Boss Baby"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABSOdvjP-MYvl6otr1nsybL4rFHAbkDuv2rDUp0maRqrL0nJYX8Ni4QnW9B2iE-PeGcUzGCQdtFYxPey__ZRlPlCSjwdBLWaASO4.jpg?r=c49",moviename:"Tom & Jerry"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABSLPVz352-RTwdUHmERXPE9jYP6sk_wTg7m92c2h_s-sV33VMT67yLQXJek9O2xdk7HJObneJoZYhgDakDfo6nv8CtZ_0RHbkls.jpg?r=969",moviename:"Spider-Man:Into The Spider-Verse"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABZ34GLuGGRGjz3HAB2zH43U6FQU7Y--TirVr8tSzuvANJw07nxySGLsPM92K8T1C3ltxl1VUAR58jeddF2qC04sz95hp_VgESY8.jpg?r=2a7",moviename:"Stuart Little 2"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABcbD0mkbPc-th_EMlnWLDribHW4ZXBrVmvY3mvNGikhRdXd9tCWasQPwkF7myxoBS7eNN5LjNFoj0RWZJXEoseA7Ve19bZWa8T8.jpg?r=6e4",moviename:"Doctor G"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABexAV_wSfoBJf_ksXkmlgs0e3QZOYTtveV6y2J7MGC17svI3zhcimQa8lkMG_NFP41ymacU_KlRx1ufjyjEHJWCIkP3g3zxRk3WM-pl_AGCxHXiEQdWdXVi0HSsdW7wGbsRQ.jpg?r=4e0",moviename:"Yes Day"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABQWIgBO_EFhOKrvusmvJ65y8sI24OEmAMpeLrnGtbfX4WasnUl3oPS1Q4Lu2ttWYw5O4WeAWN-kko_WW1qeF3PR-y-OWn3Dg7oQ4wGuVYaNMksrS439WmckoepQLc_IfaqrF.jpg?r=347",moviename:"Red Notice"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABXgJKtI3hDJ_VzmIlYLTe8YGYiP1mr9-C9jWKc9Cgds4dH-LeGfElfSasrgpVFBe6BEuxZMHCoggW9PnSeOi_g7STO7B-Fwj6oQ.jpg?r=50d",moviename:"Space Jam"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABR7YOqcZgoT0Mqr4otn8CIr1jJgBXdke6YhYsVsD-97VeDfnixTvvinEnFQO4JvrGns5C8QYZ3ZbeLu01r-JBdSzkSrbU62PZdA.jpg?r=d2a",moviename:"Dear Zindagi"}]
+
+let SuspensefulMovies =
+[{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABbK0SZnHO1LHY1iX01eKJr5vCSdtehAalWllMrN8wx_GZ0sH75cMZ1Zvd6cRT6o2TWoKBDLvkPLUqWMyvC6Ws8h9iy1rikcUpYo.jpg?r=8db",moviename:"The Forest"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABZ5QMDCrDQge7XyYgxBq7zmNPrvAVQozIZ9ZqblkV7G3BE9fr-icCMPI6docgYfGAbfikKzkOSit7cohTdileghkB_VWVTzjA5Y.jpg?r=99f",moviename:"Mili"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABb0tV5i-OKQXhejaMx623jtVFs7wUbn-D3E2yzmonWXEZxdjSWYPZML20OwaDVCw2zxNQ5GwQLbol__Y0gcXUx6SKSCA5yn1ccabXa5ZqDpxtS0b2vrEHbJQ8md9P3r5o0fx.jpg?r=8e0",moviename:"NightBooks"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABSE3AtlM3h8LOvUTtBH0K9rUyJxqbzbKxh49KYW6PJdLQ2_1i8v_bRWACUAUMiWGbTJIQbs9nrpdnUUuOk1_OL9yQT1P5IY-Fyw.jpg?r=885",moviename:"Hit:The First Case"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABTvH-KKZOptDhdnLxOuTqfD-gIcSQbc1OtZoLic7iokSCiYuTZJvISpzKxJ4Bzqt-P3KV9KZVaaTCewTsgvo_6Jtzr2dv6siqCTjJfYC5q5rDBvMeNtYNIkwPyACtFAQlVEe.jpg?r=b32",moviename:"The Mother"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABSLjZ8zyzooXHOcBEXSKnI90rWv6X6r1jUIoxJE1kJeNaf6PedajdNLKcZRD9RyGTMIaRe9Ko2uhYx1zsGgcZ4vpGlUB10k6jc8.jpg?r=8e7",moviename:"DoBaaraa"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABR4R-vv3bzxjylT8DJ2qAIFkp3k6W5xKgod2qemsuVSuUnxIFeTccTBm33slYanS0chIJMd-auvQ-R8XG3uLdoMNVnJvnkQ3I88.jpg?r=344",moviename:"The Body"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABdGmKJRrxj7d9JlI2OeFJAsVSIW3BKY0bZtEyF7TDBkCsVojrQIfPdyBv4L2IyFZQzQcnFkkFoQytEsK-RsT5v92eWTXOEKQqMU.jpg?r=74e",moviename:"Force 2"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABcFrxcVkyxy-vuDS0mnRz3c-eF8wP8iBqOlrlPuBhXsSJtPaOfLEdPjmI5f6GGrwV6fntNOwLccewDxFAC6WKpnd5sw5KgwA4OI.jpg?r=065",moviename:"Phantom"}]
+
+let ActionMovies =
+[{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABUfBoFJ9GGgUN9j2aXlD5JXGZJlwkl-Kwp3rbRdDPNCdPK8enVYjVyfJFgYVwj70gl6wFTlAOrU8FKV7Gwc04BKQ-0_UFyBoXsY.jpg?r=4b5",moviename:"Raw(Hindi)"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABcUjzHFgb4aq-ldAOLiJgBTZWSNhomBYYC-tmwn7yrEOO_h42ba43f6K5fYm0Q6cINHObchs3dFQe0tL_DqUsTPUA6KXFCXsm61SRoeEmizLqGlZOFU9GREGBKIf-qq8eh6A.jpg?r=362",moviename:"Extraction"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABadvo7k5raPlSB1iyWIJqUsTOXB9C7kkF_SW_BBcldcW0d2N55MHV2ELQFbt17B5I9X8j11RT69cKf8X9MJRojrt8sHwrMIbXIA.jpg?r=e2c",moviename:"Ghost"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABY-hEFPD6A75T0bGz5LHoA-dgm7Jj9gOQ54sO06CwUf_XrGaszGv2uLeU02LHqGWL1kFwQTkdU_ZNG6jo1d9wGNZ0xn32juuwZM.jpg?r=de4",moviename:"RRR(Hindi)"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABZCv7XAj-2is4pJztcnC0pPNcTezw5XMPjdBOxDrSyBR0UA0j4JfnYS6zKNyCij_SiV3gTuZlhfZdgIPwb9DhckJkIPPt634tHgPm5KpSx6c0VLWDf6kjX_2ICfFJfyORiJ4.jpg?r=e1d",moviename:"Drive"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABWkdmhDwEM5kzr1q2bUf2KIw_IYtD8MhZsn0Sc5mX6Lu0-H6aU3CCmi6qfznXtIlj6D-jLXuGKlqSmHWKk88O2KTNM2g4pbCQsE.jpg?r=5dd",moviename:"Dasara"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABXcbm3I5MuanXuZjZ13V3nq6wtMWJ0DYSjDka29H1b-avkLcoVZe4B1oYf7Dk6TDsuR4mV_urE0b5fTK_QwnW8pgsDrksL732yg.jpg?r=189",moviename:"Raees"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABRPA_o6Nnu5_zvhImXBQn73Vno2XbO36JlJ0aMDEOcQgKZI_oWzS4iZ1v8yoXU2a-UrE5rC_AqLvLWRaON7_PyNf2OvPzgWtsGs.jpg?r=a04",moviename:"Sooryavanshi"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABYaIR2T_Is4G9G8YKK2lPNxEUow2Fwi1KYKMvPOtRH1Bj0RYIIYJ4GQ2YoP0ULdT5m23aR_isBiGpaBzTCaJNZKExrzXWt-mvOs.jpg?r=259",moviename:"Godzilla vs Kong"}]
+
+let HorrorMovies =
+[{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABUP01a1TRGhwmZWswktC9-M65u2gcpU9ELbwrci6pDHXJUYANgMCQ2xx5dz6aeS87s_AOiwvzOYyGVvH1p_-70GUsNTdjvhyoGY.jpg?r=a14",moviename:"Ouija:Origin of Evil"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABbhpqmbIzbiwkY9AX0hgYsK2OSg9-XdIIzFG6tRlrptVs9q5WesCWjODnMcDX0LPEAv4MxavY7xe2ZV1QSFfM6a-W-tFtMV4MpJdP9pyN-Cb2LNoqWeTZCeN4cj0hU2cnxpu.jpg?r=182",moviename:"#Alive"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABR0EVuhuBR5uMuQzwtPiFSWCL0ntCLW5TjFqZmWMjocrPoLfZE8Yy8zmRaPnNYtN0toyk-zEmHoCAzhVmp5yGejqCjR2kXLrTqEZgx9HEl4swkzTKMuKQ8tLcZOtqSnXG4ro.jpg?r=724",moviename:"No One Gets Out Alive"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABStn99QM71ZkCLj5fHOa9CMjd1TN8P11Br9hPdp6x9SsoapaTS9g70RKru1Yrhd6iVEfCd_YzPX_ojFVXu6XuTLW26TdmdLpoLc.jpg?r=684",moviename:"Evil Dead"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABfqjdunnK3kaD4AoC436F8QpOyKET6_SuZVxE9omPfvoyhFYmBjr2lgyBes-3_i3f7a119Vd3PQLsF_pevhZOirNzPk3zbzftZ0.jpg?r=0e9",moviename:"Seed Of Chucky"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABViTEGabAO9RKKF1_I5w91QvSs58bEWFpP3TiXygUA3nd07jPXVjE8qlNUMOCIFsNgB6gUOJTcnkozxhYtpoHlOpDbwNB2euXJc.jpg?r=612",moviename:"Kumari"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABTr41Rmjz98XMhteC9wUYj8Ubaelr6KRSXO-GN121GwwLOXXo9GSo32H1Z4iyfWygNdKQRfky5Wqu8rdSXDj_ZMdW9ZagmhdAAqXnsJBVHxipErXNIPAruDQWAt632Zkk94Z.jpg?r=9f6",moviename:"The 8th Night"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABfJ27F4IQEnk2xrNp4nvkDTabCGOKpx19Ab91V1kqlVPbsMuVHChZhrNtHOx5uC_NsW_h0PcLYlODhfGf2eb3AnjrgSQhYST7rBcMIauDsjWOory88kGRZYC0lvvMPbdBF_n.jpg?r=5bb",moviename:"Ghist Stories"},
+{Image:"https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABeHVGAA70xQ7wqPfbAKpBQ6vRTKYIy54U-1SeG_X7gz2u26ZBky0NTk4PsNU8OBaqRWuUjvLc9Cw9BKGFO3TQ-nW8CsM9pJCJmlCNg1Jy_oTnnGH2qWOgtj2RtavqooJdR2s.jpg?r=8d0",moviename:"Choose Or Die"}]
+
+let moviesData = document.createElement("div");
+moviesData.setAttribute("class","pastMovies");
+document.querySelector(".conatiner").appendChild(moviesData);
+
+releasedInPast.forEach(function(ele){
+    let newdiv = document.createElement("div");
+    moviesData.appendChild(newdiv);
+    let image = document.createElement("img");
+    image.src = ele.Image;
+    newdiv.appendChild(image);
+
+    let name = document.createElement("p");
+    name.innerText = ele.moviename;
+    newdiv.appendChild(name);
+})
+
+let comedy = document.createElement("div");
+comedy.setAttribute("class","pastMovies");
+document.querySelector(".Comedy").appendChild(comedy);
+
+comedyMovies.forEach(function(ele){
+    let newdiv = document.createElement("div");
+    comedy.appendChild(newdiv);
+    let image = document.createElement("img");
+    image.src = ele.Image;
+    newdiv.appendChild(image);
+
+    let name = document.createElement("p");
+    name.innerText = ele.moviename;
+    newdiv.appendChild(name);
+})
+
+
+let suspens = document.createElement("div");
+suspens.setAttribute("class","pastMovies");
+document.querySelector(".Suspenseful").appendChild(suspens);
+
+SuspensefulMovies.forEach(function(ele){
+    let newdiv = document.createElement("div");
+    suspens.appendChild(newdiv);
+    let image = document.createElement("img");
+    image.src = ele.Image;
+    newdiv.appendChild(image);
+
+    let name = document.createElement("p");
+    name.innerText = ele.moviename;
+    newdiv.appendChild(name);
+})
+
+let Horror = document.createElement("div");
+Horror.setAttribute("class","pastMovies");
+document.querySelector(".Horror").appendChild(Horror);
+
+HorrorMovies.forEach(function(ele){
+    let newdiv = document.createElement("div");
+    Horror.appendChild(newdiv);
+    let image = document.createElement("img");
+    image.src = ele.Image;
+    newdiv.appendChild(image);
+
+    let name = document.createElement("p");
+    name.innerText = ele.moviename;
+    newdiv.appendChild(name);
+})
+
+
+let Action = document.createElement("div");
+Action.setAttribute("class","pastMovies");
+document.querySelector(".Action").appendChild(Action);
+
+ActionMovies.forEach(function(ele){
+    let newdiv = document.createElement("div");
+    Action.appendChild(newdiv);
+    let image = document.createElement("img");
+    image.src = ele.Image;
+    newdiv.appendChild(image);
+
+    let name = document.createElement("p");
+    name.innerText = ele.moviename;
+    newdiv.appendChild(name);
+})
